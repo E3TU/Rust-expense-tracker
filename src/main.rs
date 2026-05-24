e4b93a01f84498291433e08000e096a44b9989e9
Eetu <99178022+E3TU@users.noreply.github.com>
2026-05-25 00:38:17 +0300

    Add and list expenses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,83 @@
+use rust_decimal::Decimal;
+use std::io;
+use std::str::FromStr;
+use colored::*;
+
+struct Expense {
+    id: u64,
+    title: String,
+    amount: Decimal,
+    // date: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut expenses: Vec<Expense> = Vec::new();
+
+    let mut expense_id: u64 = 1;
+
+    loop {
+        println!("Enter an option:");
+        println!("1. Add an expense");
+        println!("2. List all expenses");
+        println!("3. Delete an expense");
+        println!("4. Quit!");
+
+        let mut option = String::new();
+
+        io::stdin()
+            .read_line(&mut option)
+            .expect("Failed to read line");
+
+        match option.trim().parse::<u32>() {
+            Ok(number) => match number {
+                1 => {
+                    println!("Enter expense title: ");
+
+                    let mut expense = String::new();
+
+                    io::stdin().read_line(&mut expense).expect("Invalid input");
+
+                    let title = expense.trim().to_string();
+
+                    expense.clear();
+
+                    println!("Enter the amount: ");
+
+                    io::stdin().read_line(&mut expense).expect("Invalid input");
+
+                    let amount = Decimal::from_str(expense.trim()).unwrap();
+
+                    expenses.push(Expense { id: (expense_id), title: title, amount: amount });
+                    
+                    expense_id += 1;
+
+                    println!("{}", "Expense added successfully!".green().bold());
+                    println!("");
+                }
+                2 => {
+                    println!("");
+                    println!("{}", "Expenses: ".green().bold());
+
+                    for (i, expense) in expenses.iter().enumerate() {
+                        println!("{}. Title: {}, Amount: {}€, (id: {})", i + 1, expense.title, expense.amount, expense.id);
+                    }
+                    println!("");
+                }
+                3 => {
+                    println!("Delete expense");
+                }
+                4 => {
+                    println!("Quitting program!!!");
+                    break;
+                }
+                _ => {
+                    println!("Enter a number between 1 and 4");
+                }
+            },
+            Err(_) => {
+                println!("Enter a valid number");
+            }
+        }
+    }
 }
